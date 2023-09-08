71229472ca1d63bf83b1794eee6ccb8ac8f7b097
gkaufman2026 <gerald.kaufman@mymail.champlain.edu>
2023-09-08 10:49:53 -0400

    Animated vertex positions

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -19,9 +19,11 @@ const char* vertexShaderSource = R"(
 	layout(location = 0) in vec3 vPos;
 	layout(location = 1) in vec4 vColor;
 	out vec4 Color;
+	uniform float _Time;
 	void main(){
 		Color = vColor;
-		gl_Position = vec4(vPos,1.0);
+		vec3 offset = vec3(0,sin(vPos.x + _Time),0)*0.5;
+		gl_Position = vec4(vPos + offset,1.0);
 	}
 )";
 
